20ccb2971c7faa466948e493a472fe0d7eefc601
poppycars22 <92829663+poppycars22@users.noreply.github.com>
2024-01-24 10:45:42 -0500

    making sure the stats use the correct player

--- Code/PSA/PSA/HealthHandlerPatch.cs
+++ Code/PSA/PSA/HealthHandlerPatch.cs
@@ -29,14 +29,14 @@ namespace PSA.Patches
             {
                 return;
             }
-            if (player.data.stats.GetAdditionalData().damageReduction > 1)
-                temp = player.data.stats.GetAdditionalData().damageReduction - (player.data.stats.GetAdditionalData().damageReduction * Math.Clamp(player.data.stats.GetAdditionalData().reductionPierce, 0, float.MaxValue));
+            if (player.data.stats.GetAdditionalData().damageReduction > 1 && damagingPlayer!=null)
+                temp = player.data.stats.GetAdditionalData().damageReduction - (player.data.stats.GetAdditionalData().damageReduction * Math.Clamp(damagingPlayer.data.stats.GetAdditionalData().reductionPierce, 0, float.MaxValue));
             else
                 temp = player.data.stats.GetAdditionalData().damageReduction;
             if (temp > 0)
                 damage /= temp;
             if (damagingPlayer != null && player.data.stats.GetAdditionalData().thorns > 0)
-                damagingPlayer.data.healthHandler.DoDamage(damage * player.data.stats.GetAdditionalData().thorns*Math.Clamp(player.data.stats.GetAdditionalData().thornsPercent,0,float.MaxValue), Vector2.down, Color.green, null, null, false, false, true);
+                damagingPlayer.data.healthHandler.DoDamage(damage * player.data.stats.GetAdditionalData().thorns*Math.Clamp(damagingPlayer.data.stats.GetAdditionalData().thornsPercent,0,float.MaxValue), Vector2.down, Color.green, null, null, false, false, true);
             if (damagingPlayer != null && damagingPlayer.data.stats.GetAdditionalData().thorns > 0 && damagingPlayer.data.stats.GetAdditionalData().selfThorns)
                 damagingPlayer.data.healthHandler.DoDamage(damage * (damagingPlayer.data.stats.GetAdditionalData().thorns*Math.Clamp(damagingPlayer.data.stats.GetAdditionalData().selfThornsPercent,0,float.MaxValue)), Vector2.down, Color.green, null, null, false, false, true);
         }
